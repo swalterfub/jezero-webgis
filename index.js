fb2c36666a4a838d578476c0c9dc179f11c8cca1
Sebastian Walter <s.walter@fu-berlin.de>
2021-01-27 22:07:20 +0100

    add custom projection (lot of stuff on server

--- index.js
+++ index.js
@@ -1,31 +1,68 @@
 import 'ol/ol.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import TileWMS from "ol/source/TileWMS";
+import { defaults as defaultControls, ScaleLine } from "ol/control";
 
 import LayerSwitcher from 'ol-layerswitcher';
+
+import { register } from "ol/proj/proj4";
+import { Projection, getTransform, get } from "ol/proj";
+import { getDistance } from "ol/sphere";
+import proj4 from "proj4";
+
+proj4.defs("EPSG:49901", "+proj=longlat +R=3396190 +no_defs ");
+proj4.defs(
+  "EPSG:49911",
+  "+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +R=3396190 +units=m +no_defs "
+);
+register(proj4);
+//https://maps.planet.fu-berlin.de/jez-bin/wms?
+var projection = new Projection({
+  code: "EPSG:49911",
+  global: true,
+  //extent: [4000000, 0, 4500000, 500000],
+  extent: [-10668848.652, -5215881.563, 10668848.652, 5215881.563],
+  getPointResolution: function(resolution, point) {
+    var toEPSG49901 = getTransform(get("EPSG:49911"), get("EPSG:49901"));
+    var vertices = [ point[0] - resolution / 2, point[1], point[0] + resolution / 2, point[1] ];
+    vertices = toEPSG49901(vertices, vertices, 2);
+    return getDistance(vertices.slice(0, 2), vertices.slice(2, 4), 3396190);
+  }
+});
 
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
-      title: "OSM",
-      source: new OSM()
-    })
+      title: "CTX",
+      source: new TileWMS({
+        url: "https://maps.planet.fu-berlin.de/jez-bin/wms?",
+        params: { LAYERS: "CTX-hsv" }
+      })
+    }),
+    new TileLayer({
+      title: "HMC",
+      source: new TileWMS({
+        url: "https://maps.planet.fu-berlin.de/eqc-bin/wms?",
+        params: { LAYERS: "HMChsvlog" }
+      })
+    }),
   ],
+  controls: defaultControls().extend([
+    new ScaleLine({
+      units: "metric"
+    })
+  ]),
   view: new View({
     center: [0, 0],
-    zoom: 0
+    zoom: 0,
+    projection: projection
   })
 });
 
-var layerSwitcher = new LayerSwitcher();
-//map.addControl(layerSwitcher);
+//var layerSwitcher = new LayerSwitcher();
 
-// Get out-of-the-map div element with the ID "layers" and renders layers to it.
-// NOTE: If the layers are changed outside of the layer switcher then you
-// will need to call ol.control.LayerSwitcher.renderPanel again to refesh
-// the layer tree. Style the tree via CSS.
 var sidebar = new ol.control.Sidebar({
   element: 'sidebar',
   position: 'left'
