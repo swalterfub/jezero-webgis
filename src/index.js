f38919c1d76bbc10af312c9de22e61bfe8bac83c
Sebastian Walter <s.walter@fu-berlin.de>
2021-02-12 19:28:16 +0100

    add info/help

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ textarray[6]=`
 <h3>Mars 2020 Rover &ldquo;Perseverance&rdquo; landing site</h3><p>The NASA Mars 2020 mission is en route to Mars since July 30th 2020. On board: The NASA rover “Perseverance”, the most complex equipment ever sent to Mars. Besides numerous scientific instruments, it will carry containers for a drill core sample collection that will be left on Mars for a later return to Earth carried out by follow-up missions planned for the 2030s. The approximately one metric ton heavy vehicle is set to land on 18 February 2021 at 21:55 CET in Jezero crater and will then start the search for traces of microbial life. The working group at Freie Universitaet Berlin is involved with Prof. Jaumann, serving as Co-Investigator on the Mastcam Z instrument. “Ingenuity” is the name of a 1.8 kg heavy helicopter drone onboard the rover that will be used as near field reconnaissance instrument.</p>
 `
 textarray[0]=`
-<h3>Outflow channel</h3><p>The outflow channel Pliva Vallis breaks through the eastern crater rim surpassing a swell. Using altitude measurements from crater rim, crater floor, swell and upper edge of the delta, the potential depth of the lake can be deduced, resulting in roughly 250 m water depth. This would result in a minimum lake water volume of 220 km3 and a maximum of 340 km3 (see corresponding images). However, as the deltas have been severely eroded since water activity ceased approximately 3.8 billion years ago, it is no longer possible to make precise assessments. Also, the crater was filled with very extensive lava flows from the nearby Syrtis Major volcanic region about 300 million years later.</p>
+<h3>Outflow channel</h3><p>The outflow channel Pliva Vallis breaks through the eastern crater rim surpassing a swell. Using altitude measurements from crater rim, crater floor, swell and upper edge of the delta, the potential depth of the lake can be deduced, resulting in roughly 250 m water depth. This would result in a minimum lake water volume of 220 km3 and a maximum of 340 km3. However, as the deltas have been severely eroded since water activity ceased approximately 3.8 billion years ago, it is no longer possible to make precise assessments. Also, the crater was filled with very extensive lava flows from the nearby Syrtis Major volcanic region about 300 million years later.</p>
 `
 textarray[2]=`
 <h3>On top of the delta</h3><p>Within the delta deposits, numerous water-bearing minerals have been found, indicating liquid water was once present for an extended period of time.</p>
@@ -254,12 +254,20 @@ const map = new Map({
         params: { LAYERS: "channels" }
       })
     }),
-    new TileLayer({
+    new TileLayer({ 
       title: "Possible paleo lake-level",
       visible: false,
       source: new TileWMS({
         url: "https://maps.planet.fu-berlin.de/jez-bin/wms?",
         params: { LAYERS: "lake" }
+      }),
+    }),
+    new TileLayer({
+      title: "Landing ellipse",
+      visible: true,
+      source: new TileWMS({
+        url: "https://maps.planet.fu-berlin.de/jez-bin/wms?",
+        params: { LAYERS: "landingellipse" }
       }),
     }),
     poi,
